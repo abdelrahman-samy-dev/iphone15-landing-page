02fe073a898cde1c2f698fa988cbc82a317b5d2d
abdelrahman-samy-dev <abdelrahman.samy.dev@outlook.com>
2025-06-06 10:50:22 +0300

    Updated base url

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/iphone-15/',
+  base: './',
   plugins: [tailwindcss(), react(), sentryVitePlugin({
     org: "jsm-jjn",
     project: "javascript-react"
